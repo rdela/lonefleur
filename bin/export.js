a0874fc8d3615b11b313604c79d96441dcc56303
Ricky de Laveaga <git@rdela.com>
2020-01-24 20:29:42 -0800

    export todo

--- bin/export.js
+++ bin/export.js
@@ -1,1 +1,12 @@
-export
+/*
+
+TODO
+write export script to pull down fresh export with `contentful-export`
+https://github.com/contentful/contentful-export
+(already installed)
+yarn add contentful-export
+
+or alternatively CLI
+https://www.contentful.com/developers/docs/tutorials/cli/import-and-export/
+
+*/
